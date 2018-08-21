91f8dbf7f4c91d679943bfe16fdd7f5554554884
Hari Rallapalli <hari.rallapalli@gmail.com>
2018-08-21 13:25:51 -0400

    new file:   20180802_145925_20180802_HR_T1mapping_NOMN1_1_1/9/matlabRecon.tif  	modified:   BrukerImageImporter.m  	new file:   pvtools/BrukerExample.m  	new file:   pvtools/Contents.m  	new file:   pvtools/Manual.pdf  	new file:   pvtools/addBrukerPaths.m  	new file:   pvtools/datatypes/BrukerDataSuperclass.m  	new file:   pvtools/datatypes/CKDataObject.m  	new file:   pvtools/datatypes/FrameDataObject.m  	new file:   pvtools/datatypes/ImageDataObject.m  	new file:   pvtools/datatypes/RawDataObject.m  	new file:   pvtools/functions/additional/FrameGroups_Transposition/bruker_getFrameGroupParams.m  	new file:   pvtools/functions/additional/FrameGroups_Transposition/bruker_getTranspositionFrame.m  	new file:   pvtools/functions/additional/FrameGroups_Transposition/bruker_setTranspositionFrame.m  	new file:   pvtools/functions/additional/RECO/bruker_Reco.m  	new file:   pvtools/functions/additional/RECO/bruker_freqimage.m  	new file:   pvtools/functions/additional/RECO/bruker_imagefreq.m  	new file:   pvtools/functions/additional/bruker_addParamValue.m  	new file:   pvtools/functions/additional/bruker_colorgray.m  	new file:   pvtools/functions/additional/bruker_combineStructs.m  	new file:   pvtools/functions/additional/bruker_findDataname.m  	new file:   pvtools/functions/additional/bruker_getSelectedReceivers.m  	new file:   pvtools/functions/additional/bruker_requires.m  	new file:   pvtools/functions/additional/bruker_version.m  	new file:   pvtools/functions/additional/exportToPV/bruker_genSlopeOffsMinMax.m  	new file:   pvtools/functions/additional/exportToPV/bruker_genVisu.m  	new file:   pvtools/functions/additional/exportToPV/bruker_write2dseq.m  	new file:   pvtools/functions/additional/exportToPV/bruker_writeVisu.m  	new file:   pvtools/functions/brViewer.m  	new file:   pvtools/functions/convertFrameToCKData.m  	new file:   pvtools/functions/convertRawToFrame.m 	new file:   pvtools/functions/readBruker2dseq.m 	new file:   pvtools/functions/readBrukerParamFile.m 	new file:   pvtools/functions/readBrukerRaw.m

--- BrukerImageImporter.m
+++ BrukerImageImporter.m
@@ -7,18 +7,34 @@ close all
 
 % read in fid file as int32 with file header
 [FID, FIDPATH] = uigetfile('*.*');
- fi=fopen([FIDPATH FID], 'rb', 'ieee-le');
+path = [FIDPATH FID];
+fi=fopen(path, 'rb', 'ieee-le');
 [data, cnt]=fread(fi, inf,'int32');
 fclose(fi);
-
 % reshape the data and construct an array of complex numbers
-  data2=reshape(data, 2, cnt/2);
-  cdata=complex(data2(1,:), data2(2,:));
+  data1 = data(1:4:end);
+  data2 = data(2:4:end);
+  data3 = data(3:4:end);
+  data4 = data(4:4:end);
+
+  data1_real = data1(1:2:end);
+  data1_imag = data1(2:2:end);
+  
+cdata=complex(data1_real, data1_imag);
  
 % now cdata is an array of complex number, you can reshape it for following fft reconstruction
 
 %% Reconstruct
-kspace_temp = reshape(cdata,128,128,80);
+
+matrix_size = bruker_method(FIDPATH, 'acqp', 'ACQ_size');
+
+num_echoes = bruker_method(FIDPATH, 'method', 'PVM_NEchoImages');
+num_exp   =  bruker_method(FIDPATH, 'method', 'PVM_NEchoImages');
+num_coils =  bruker_method(FIDPATH, 'method', 'PVM_EncNReceivers');
+
+
+reshape_size = [matrix_size,1,num_coils,num_exp,num_echoes];
+kspace_temp = reshape(cdata,matrix_size(1),matrix_size(2),num_coils,25);
 kspace_size = size(kspace_temp);
 
 im_recon =  fftshift(fftn(fftshift(kspace_temp)))*1/sqrt(length(kspace_temp(:)));
@@ -34,6 +50,6 @@ data = uint16(abs(im_recon));
 outputFileName = [FIDPATH 'matlabRecon.tif'];
 
 
-for k = 1:size(data,3);
+for k = 1:size(data,3)
     imwrite(data(:,:,k),outputFileName,'writemode','append','compression','none')
 end
